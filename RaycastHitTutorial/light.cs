e9a4e41b769e8e3dc683e77a0b4a3395591930e4
Abdullah Kaşgar <abdullahkasgar2494@gmail.com>
2023-02-13 17:31:56 +0300

    Update light.cs

--- RaycastHitTutorial/light.cs
+++ RaycastHitTutorial/light.cs
@@ -21,10 +21,28 @@ public class light : MonoBehaviour
         // Vector3 forward = transform.TransformDirection(Vector3.left) * 10;
         // Debug.DrawRay(transform.position, forward, Color.green);
 
-        if (Physics.Raycast(transform.position, Vector3.back, out _object, 10.0f))
+        /*
+         if (Physics.Raycast(transform.position, Vector3.back, out _object, 10.0f))
         {
             if (_object.collider.gameObject.tag == "Player")
-                 Debug.Log("Light touched the sphere!");
+            {
+                // Debug.Log("Light touched the sphere!");
+                //_object.collider.gameObject.GetComponent<MeshRenderer>().enabled = false;
+                _object.collider.gameObject.SetActive(false);
+            }
+        }
+        */
+
+        //Ray sendLight = GameObject.Find("Main Camera").GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        Ray sendLight = GameObject.Find("Main Camera").GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(sendLight, out _object))
+        {
+            if (_object.collider.gameObject.tag == "Player")
+            {
+                Debug.Log("Object Touched!");
+            }
         }
     }
 }
